cfb221366b186ed32c00e487d7282e7a38e6ebca
Shivam Singh <sshivamsingh440@gmail.com>
2021-08-04 20:54:50 +0530

    Updates

---
+++ precache-manifest.9ff103a3f0f4d23beff3ed7ae835dadd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93352a5c143e133f52d282ee00c1917",
+    "url": "/covid19_tracker/index.html"
+  },
+  {
+    "revision": "90b9775ff97258dda835",
+    "url": "/covid19_tracker/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "7ef7f4b177587f740ce6",
+    "url": "/covid19_tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "90b9775ff97258dda835",
+    "url": "/covid19_tracker/static/js/2.414fa468.chunk.js"
+  },
+  {
+    "revision": "a8b062ea113d5347eaa037e7d1931e01",
+    "url": "/covid19_tracker/static/js/2.414fa468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef7f4b177587f740ce6",
+    "url": "/covid19_tracker/static/js/main.1d16ebc6.chunk.js"
+  },
+  {
+    "revision": "99acc629d9d625b6766e",
+    "url": "/covid19_tracker/static/js/runtime-main.82455dbe.js"
+  }
+]);
